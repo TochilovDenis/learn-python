d3ce25ca7b59eeb5d23d4377c1744f574c9f18ea
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-09 20:17:54 +0300

    update task_01
     2024.10.09.11

--- 2024.10.09/task_01/main.py
+++ 2024.10.09/task_01/main.py
@@ -1,8 +1,11 @@
-def text_split(s: str, separator: str) -> list[str]:
-    return s.split(separator)
+INITIAL_STR: str = "Строка1; Строка2. Строка3."
+SEPARATORS: list =  [';', '.']
+
+def text_split(s: str, separators: str) -> list[str]:
+    for separator in separators:
+        return s.split(separator)
 
 
 if __name__ == '__main__':
-    text = "Hello Dex"
-    print(text_split(text, '=)'))
+    print(text_split(INITIAL_STR, SEPARATORS))
 
