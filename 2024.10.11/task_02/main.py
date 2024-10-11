432dc1419358eaca8274a210c5c7280584c22d92
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-11 20:35:57 +0300

    added strip()
    Теперь мы на вход получаем список строк и список сепараторов и должны вернуть список строк.
    
    Пример.
    
    Получаем сепараторы: .;!?
    Входной список: ['Вышел корень', ' из т', 'умана; ', 'Вынул но', 'жик из кармана. ', 'Раз, два, всё?..']
    Выход: ['Вышел корень из тумана; ', 'Вынул ножик из кармана. ', 'Раз, два, всё?..']

--- 2024.10.11/task_02/main.py
+++ 2024.10.11/task_02/main.py
@@ -5,13 +5,13 @@ def similar_segment(segments: list[str], separators: set[str]) -> list[str]:
     result: list[str] = ['']
     for segment in segments:
         result[-1] += segment
-        if segment[-1] in separators and segment is not segments[-1]:
+        if segment.strip()[-1] in separators and segment is not segments[-1]:
             result.append('')
 
     return result
 
 
 if __name__ == '__main__':
-    initial_list_str: list[str] = ['Дем', 'онстр', 'ация!']
-    seps = set('!?.,')
+    initial_list_str: list[str] =  ['Вышел корень из тумана; ', 'Вынул ножик из кармана. ', 'Раз, два, всё?..']
+    seps = set('!?.,;')
     print(similar_segment(initial_list_str, seps))
