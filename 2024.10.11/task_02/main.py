d7721fed032fecf9a6f0abf897089b9cafbff1d1
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-11 20:31:43 +0300

    Update main.py

--- 2024.10.11/task_02/main.py
+++ 2024.10.11/task_02/main.py
@@ -1,27 +1,17 @@
-def text_split_by_separators(texts: list[str], separators: list[str]) -> list[str]:
-    """
-    Разделяет входную строку по указанным разделителям.
-    :param texts: Входная строка для разделения.
-    :param separators: Список разделителей для использования.
-    :return list[str]: Список строк, полученных после разделения.
-    """
-    result: list[str] = ['']
-    waiting_for_separator = True
-    for symbol in texts:
-        if waiting_for_separator:
-            if symbol in separators:
-                waiting_for_separator = False
-        else:
-            if not symbol in separators and symbol != ' ':
-                result.append('')
-                waiting_for_separator = True
+def similar_segment(segments: list[str], separators: set[str]) -> list[str]:
+    if not segments:
+        return []
 
-        result[-1] += symbol
+    result: list[str] = ['']
+    for segment in segments:
+        result[-1] += segment
+        if segment[-1] in separators and segment is not segments[-1]:
+            result.append('')
 
     return result
 
 
 if __name__ == '__main__':
-    initial_list_str: list[str] = ['Дем', 'онстра', 'ция!']
-    seps: list[str] = ['.', ',', '?', '!']
-    print(text_split_by_separators(initial_list_str, seps))
+    initial_list_str: list[str] = ['Дем', 'онстр', 'ация!']
+    seps = set('!?.,')
+    print(similar_segment(initial_list_str, seps))
