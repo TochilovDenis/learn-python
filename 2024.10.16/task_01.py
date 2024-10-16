3e96d9dc76e6c9799e686ed3b691607192e4229d
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-16 21:54:19 +0300

    Update task_01 choices()
    - Метод choices() выбирает случайные элементы из заданного набора (population).
    - Он может выбирать один или несколько элементов (определенных параметром k).
    - Если population содержит повторяющиеся элементы, они могут быть выбраны более одного раза.
    
    Метод choices() является мощным инструментом для генерации случайных выборок в Python, особенно полезным в задачах, требующих случайного выбора элементов из заданного набора.

--- 2024.10.16/task_01.py
+++ 2024.10.16/task_01.py
@@ -2,18 +2,20 @@
 Задача 2024.10.16.01
 Написать функцию для генерации строки из n строчных латинских букв
 """
-from random import choice
+from random import choices
 from string import ascii_lowercase
 
 
 def generate_random_str(n: int) -> str:
-    chars = ascii_lowercase
-    result = []
-    for _ in range(n):
-        char = choice(chars)
-        result.append(char)
-    return ''.join(result)
+    # chars = ascii_lowercase
+    # result = []
+    # for _ in range(n):
+    #     char = choice(chars)
+    #     result.append(char)
+    # return ''.join(result)
     # return ''.join(choice(ascii_lowercase) for _ in range(n))
+    return ''.join(choices(population=ascii_lowercase, k=n))
+
 
 if __name__ == '__main__':
     print(generate_random_str(10))
