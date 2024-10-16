8e3ba26b06f5960284735774b48ba39aaeddc8f8
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-16 19:01:04 +0300

    Update task_01.py

--- 2024.10.16/task_01.py
+++ 2024.10.16/task_01.py
@@ -3,11 +3,11 @@
 Написать функцию для генерации строки из n строчных латинских букв
 """
 from random import choice
-from string import ascii_letters
+from string import ascii_lowercase
 
 
 def generate_random_str(n: int) -> str:
-    return ''.join(choice(ascii_letters) for _ in range(n))
+    return ''.join(choice(ascii_lowercase) for _ in range(n))
 
 if __name__ == '__main__':
     print(generate_random_str(10))
