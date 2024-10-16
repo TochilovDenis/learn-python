c28560b8e8d876910549adf1e24d4cfb7b6690d4
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-16 18:47:08 +0300

    task_01 done

---
+++ 2024.10.16/task_01.py
@@ -0,0 +1,13 @@
+"""
+Задача 2024.10.16.01
+Написать функцию для генерации строки из n строчных латинских букв
+"""
+from random import choice
+from string import ascii_letters
+
+
+def generate_random_str(n: int) -> str:
+    return ''.join(choice(ascii_letters) for _ in range(n))
+
+if __name__ == '__main__':
+    print(generate_random_str(10))
