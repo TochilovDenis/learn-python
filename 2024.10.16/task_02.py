1067eaf1b0f31ed27999d6e49593f9c348007891
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-16 19:02:08 +0300

    task_02 done

---
+++ 2024.10.16/task_02.py
@@ -0,0 +1,19 @@
+"""
+Задача 2024.10.16.02
+
+Написать функцию для генерации строки из n строчных и заглавных латинских букв
+Заглавные должны добавляться в строку только если параметр use_uppercase в функции равен True.
+А если параметр use_uppercase не указан при использовании функции, то генерируем строку только из строчных букв.
+"""
+from random import choice
+from string import ascii_lowercase, ascii_uppercase
+
+
+def generate_random_str(n: int, use_uppercase: bool = False) -> str:
+    chars: str = ascii_lowercase
+    if use_uppercase:
+        chars += ascii_uppercase
+    return ''.join(choice(chars) for _ in range(n))
+
+if __name__ == '__main__':
+    print(generate_random_str(10, True))
