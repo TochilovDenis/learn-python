e5024c98bc50ffe3d27125c5a518d7ad0843ceed
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-18 20:01:57 +0300

    Update task_03.py

--- 2024.10.18/task_03.py
+++ 2024.10.18/task_03.py
@@ -6,10 +6,10 @@ choice_lang = int(input("На каком языке вы хотите цитат
 url = 'http://api.forismatic.com/api/1.0/' # создал и инициировал переменную url
 payload = {                 # уточнённые настройки нашего запроса
     'method': 'getQuote',   # мы указываем, что метод - это getQuote или ПолучитьЦитату
-    'format': 'json',       # мы указываем, что формат - это json
+    'format': 'text',       # мы указываем, что формат - это json
     'lang': LANG[choice_lang]}   # мы указываем, что нам нужна цитата на русском языке
 res = requests.get(url, params=payload) # создал переменную res и сохранил в неё результат работы функции requests.get
 # res - это response - ответ
-data = res.json() # создал переменную data и сохранил в неё результат работы функции res.json
+data = res.text # создал переменную data и сохранил в неё результат работы функции res.text
 
 print(data) # вывел data на экран
