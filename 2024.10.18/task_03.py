da2795f7440d52c00a6cdc4a752fad5c26d6bbdd
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-18 19:56:09 +0300

    task_03 done

---
+++ 2024.10.18/task_03.py
@@ -0,0 +1,15 @@
+import requests  # импорт библиотеки requests
+
+LANG: dict[int, str] = {0: 'ru', 1: 'en'}
+choice_lang = int(input("Выбор(0-ru, 1-en): "))
+
+url = 'http://api.forismatic.com/api/1.0/' # создал и инициировал переменную url
+payload = {                 # уточнённые настройки нашего запроса
+    'method': 'getQuote',   # мы указываем, что метод - это getQuote или ПолучитьЦитату
+    'format': 'json',       # мы указываем, что формат - это json
+    'lang': LANG[choice_lang]}   # мы указываем, что нам нужна цитата на русском языке
+res = requests.get(url, params=payload) # создал переменную res и сохранил в неё результат работы функции requests.get
+# res - это response - ответ
+data = res.json() # создал переменную data и сохранил в неё результат работы функции res.json
+
+print(data) # вывел data на экран
